2bb562e67a32e7c4ce090516f71ef6cbaece2c66
Julio MS <saitodisse@gmail.com>
2025-03-13 15:26:14 -0300

    refactor: remove legacy materials tree implementation and associated tests for cleaner codebase

--- src/interfaces/Maybe.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Utility type representing a value that may be null.
- * Used throughout the system to explicitly mark optional values.
- * @template T The type of the value that may be null
- */
-export type Maybe<T> = T | null;
