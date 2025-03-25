08824835a10c5ce1dd7a5bc597f0b9af059bea5d
Julio MS <saitodisse@gmail.com>
2025-03-25 00:22:13 -0300

    fix _layout

--- site/routes/_layout.tsx
+++ site/routes/_layout.tsx
@@ -4,20 +4,29 @@ export default function Layout({ Component, state }: PageProps) {
   return (
     <div className="min-h-screen bg-gray-50">
       <nav className="bg-white shadow-md fixed top-0 w-full z-10">
-        <div className="container mx-auto max-w-4xl px-4 py-3 flex items-center justify-between">
-          <div className="flex items-center space-x-8">
-            <a href="/" className="text-xl font-medium text-gray-800">
-              BOM recipe calculator
-            </a>
-            <a
-              href="/products/list-products"
-              className="text-gray-600 hover:text-gray-800"
-            >
-              Products
-            </a>
-            <a href="/doc" className="text-gray-600 hover:text-gray-800">
-              Documentation
-            </a>
+        <div className="container mx-auto px-4 py-3 flex items-center justify-between">
+          <div className="flex items-center space-x-4 space-between w-full">
+            <div className="flex items-center space-x-4 w-full">
+              <a href="/" className="text-xl font-medium text-gray-800">
+                BOM recipe calculator
+              </a>
+              <a
+                href="/products/list-products"
+                className="text-gray-600 hover:text-gray-800 grow"
+              >
+                Products
+              </a>
+            </div>
+            <div className="flex items-center space-x-4">
+              <a
+                href="https://github.com/saitodisse/bom-recipe-calculator"
+                className="text-gray-600 hover:text-gray-800"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Github
+              </a>
+            </div>
           </div>
         </div>
       </nav>
